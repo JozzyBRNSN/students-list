c88077e6173941a6475d255840cb55c0874a277f
JozzyBRNSN <makavelidmityr313@gmail.com>
2024-06-02 12:23:39 +0300

    added boodstrap library in project

--- js/students.js
+++ js/students.js
@@ -1,26 +1,25 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let main = document.getElementById("main");
-  let container = document.getElementById("container");
+let main = document.getElementById("main");
+let container = document.getElementById("container");
 
-  let form = document.createElement("form");
-  let studentName = document.createElement("input");
-  let faculty = document.createElement("input");
-  let startDate = document.createElement("input");
-  let finishDate = document.createElement("input");
+let form = document.createElement("form");
+let studentName = document.createElement("input");
+let faculty = document.createElement("input");
+let startDate = document.createElement("input");
+let finishDate = document.createElement("input");
 
-  studentName.type = "text";
-  faculty.type = "text";
-  startDate.type = "date";
-  finishDate.type = "date";
+studentName.type = "text";
+faculty.type = "text";
+startDate.type = "date";
+finishDate.type = "date";
 
-  studentName.placeholder = "ФИО студента";
-  faculty.placeholder = "Факультет";
+studentName.placeholder = "ФИО студента";
+faculty.placeholder = "Факультет";
 
-  form.appendChild(studentName);
-  form.appendChild(faculty);
-  form.appendChild(startDate);
-  form.appendChild(finishDate);
-  container.appendChild(form);
-  main.appendChild(container);
+form.appendChild(studentName);
+form.appendChild(faculty);
+form.appendChild(startDate);
+form.appendChild(finishDate);
+container.appendChild(form);
+main.appendChild(container);
 
-});
+
