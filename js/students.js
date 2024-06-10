c377c95ed606699e92a3581d38becb77593909ef
JozzyBRNSN <makavelidmityr313@gmail.com>
2024-06-10 12:09:03 +0300

    refactoring data of obj

--- js/students.js
+++ js/students.js
@@ -16,76 +16,91 @@ function formatDate(date) {
 
 // Функция добавления даты поступления в интервале от 2000 до нынешнего года
 function minMaxStartValue(year) {
-    if (
-      year > new Date().getFullYear() ||
-      year < new Date(2000)
-    ) {
-      return alert("Введите корректную дату");
-    } else {
-      return year;
-    }
-  };
+  if (year > new Date().getFullYear() || year < new Date(2000)) {
+    alert("Введите корректную дату");
+  } else {
+    year;
+  }
+}
 
 // Функция создания нового студента
-function $getNewStudent(studObj) {
-  const $dataList = document.createElement("tr");
-  const $FIO = document.createElement("td");
-  const $birthday = document.createElement("td");
-  const $faculty = document.createElement("td");
-  const $start = document.createElement("td");
+function getNewStudent(studObj) {
+  const dataList = document.createElement("tr");
+  const FIO = document.createElement("td");
+  const birthday = document.createElement("td");
+  const faculty = document.createElement("td");
+  const start = document.createElement("td");
 
-  $FIO.textContent = `${studObj.surname} ${studObj.name} ${studObj.lastname}`;
-  $birthday.textContent = formatDate(studObj.birthday);
-  $faculty.textContent = studObj.faculty;
-  $start.textContent = studObj.start;
+  FIO.textContent = `${studObj.surname} ${studObj.name} ${studObj.lastname}`;
+  birthday.textContent = formatDate(studObj.birthday);
+  faculty.textContent = studObj.faculty;
+  start.textContent = studObj.start;
 
-  $dataList.append($FIO, $birthday, $faculty, $start);
-  return $dataList;
+  dataList.append(FIO, birthday, faculty, start);
+  return dataList;
 }
 
 function render(arr) {
   let copyArr = [...arr];
 
-  const $studTable = document.getElementById("stud-table");
+  const studTable = document.getElementById("stud-table");
 
-  $studTable.innerHTML = "";
+  studTable.innerHTML = "";
   for (const studObj of copyArr) {
-    const $newStudTable = $getNewStudent(studObj);
-    $studTable.append($newStudTable);
+    const newStudTable = getNewStudent(studObj);
+    studTable.append(newStudTable);
   }
 }
 
 render(studentsList);
 // Функция добавления нового студента в массив
-const $btn = document.getElementById("add-btn");
+const btn = document.getElementById("add-btn");
+const input = document.querySelectorAll(".form-control");
 
-let name = document.getElementById("name-inp");
-let surname = document.getElementById("surname-inp");
-let lastname = document.getElementById("lastname-inp");
-let birthday = document.getElementById("birthday-inp");
-let start = document.getElementById("start-inp");
-let faculty = document.getElementById("faculty-inp");
+btn.disabled = true;
+
+// input.forEach((inp) => {
+//   inp.addEventListener("input", function () {
+//     if (this.value.length !== 0) {
+//       btn.disabled = false;
+//     } else {
+//       btn.disabled = true;
+//     } 
+//   });
+// });
+
+document.getElementById("start-inp").addEventListener("input", function () {
+    if (this.value.length !== 0) {
+      btn.disabled = false;
+    } else {
+      btn.disabled = true;
+    }
+  });
 
 document
   .getElementById("add-form")
   .addEventListener("submit", function (event) {
     event.preventDefault();
+
     let newStudentObj = {
-      name: name.value,
-      surname: surname.value,
-      lastname: lastname.value,
-      birthday: new Date(birthday.value),
-      start: minMaxStartValue(parseInt(start.value)),
-      faculty: faculty.value,
+      name: document.getElementById("name-inp").value,
+      surname: document.getElementById("surname-inp").value,
+      lastname: document.getElementById("lastname-inp").value,
+      birthday: new Date(document.getElementById("birthday-inp").value),
+      start: minMaxStartValue(
+        parseInt(document.getElementById("start-inp").value)
+      ),
+      faculty: document.getElementById("faculty-inp").value,
     };
- 
+
     studentsList.push(newStudentObj);
     render(studentsList);
 
-    name.value = "";
-    surname.value = "";
-    lastname.value = "";
-    birthday.value = "";
-    start.value = "";
-    faculty.value =  "";
+    document.getElementById("name-inp").value = "";
+    document.getElementById("surname-inp").value = "";
+    document.getElementById("lastname-inp").value = "";
+    document.getElementById("birthday-inp").value = "";
+    document.getElementById("start-inp").value = "";
+    document.getElementById("faculty-inp").value = "";
+    btn.disabled  =  true;
   });
